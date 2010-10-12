2dfc2bee355d5399038bfd6cb00d8dd44202f14f
Paul Adam Davis <pauladamdavis@gmail.com>
2010-10-12 06:05:16 -0700

    Added new function to alter the default [...] that comes from the_excerpt() function if the post is long

--- functions.php
+++ functions.php
@@ -5,5 +5,12 @@
     	global $wp_query;
     	return ($wp_query->max_num_pages > 1);
     }
+
+    // Modifies the default [...] to say something a little more useful.
+    function new_excerpt_more($more) {
+        global $post;
+        return '&nbsp;<a href="'. get_permalink($post->ID) . '">' . 'Read more' . '</a>';
+    }
+    add_filter('excerpt_more', 'new_excerpt_more');
 	
 ?>
