0d82773bfa6ebeef46412a4a9a694aa1ac4da50f
Paul Adam Davis <pauladamdavis@gmail.com>
2012-02-21 17:45:07 +0000

    Added function to get featured images in feeds

--- functions.php
+++ functions.php
@@ -58,3 +58,15 @@
     
     // Support menus
     add_theme_support('menus');
+
+    // Add featured image to feeds
+    // THIS INCLUDES THE THUMBNAIL IN OUR RSS FEED
+    function insertThumbnailRSS($content) {
+        global $post;
+        if ( has_post_thumbnail( $post->ID ) ){
+            $content = '' . get_the_post_thumbnail( $post->ID, 'thumbnail', array( 'alt' => get_the_title(), 'title' => get_the_title(), 'style' => 'float:right;' ) ) . '' . $content;
+        }
+        return $content;
+    }
+    add_filter('the_excerpt_rss', 'insertThumbnailRSS');
+    add_filter('the_content_feed', 'insertThumbnailRSS');
